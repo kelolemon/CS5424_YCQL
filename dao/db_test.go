eb4edd7241d9fc9ac28461e4a4163c425b597127
Liuyy99 <yiyang7270@gmail.com>
2022-10-16 23:12:41 +0800

    test insertion

--- dao/db_test.go
+++ dao/db_test.go
@@ -2,6 +2,7 @@ package dao
 
 import (
 	"cs5234/client"
+	"cs5234/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/yugabyte/gocql"
 	"testing"
@@ -16,6 +17,39 @@ func TestDBConnection(t *testing.T) {
 		defer session.Close()
 		var count int32
 		err = session.Query(`SELECT count(*) FROM warehouse`).Consistency(gocql.One).Scan(&count)
+		assert.NoError(t, err)
+	}
+}
+
+func TestInsertion(t *testing.T) {
+	client.InitDB()
+	session, err := client.DBCluster.CreateSession()
+	assert.NoError(t, err)
+
+	if session != nil {
+		defer session.Close()
+		warehouse := common.Warehouse{
+			ID:      1,
+			Name:    "sxvnjhpd",
+			Street1: "dxvcrastvybcwvmgnyk",
+			Street2: "xvzxkgxtspsjdgylue",
+			City:    "qflaqlocfljbepowfn",
+			State:   "OM",
+			Zip:     "123456789",
+			Tax:     0.0384,
+
+			Ytd: 300000.0,
+		}
+		err = session.Query(`INSERT INTO warehouse(w_id, w_name, w_street_1, w_street_2, w_city, w_state, w_zip, w_tax, w_ytd) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+			&warehouse.ID,
+			&warehouse.Name,
+			&warehouse.Street1,
+			&warehouse.Street2,
+			&warehouse.City,
+			&warehouse.State,
+			&warehouse.Zip,
+			&warehouse.Tax,
+			&warehouse.Ytd).Exec()
 		assert.NoError(t, err)
 	}
 }
