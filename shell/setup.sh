bef53dbce058aa0e4aeb8ca7ad029a72d0dc412b
kelolemon <cutesinapis@gmail.com>
2022-10-24 16:59:40 +0800

    feat: add shell folder to prepare benchmark

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,7 +1,7 @@
 echo "umask u=rwx,g=,o=" >> ${HOME}/.bashrc
-mkdir -p ${HOME}/.ssh
+mkdir -p "${HOME}"/.ssh
 mkdir -p /temp/cs5424c
-cd ${HOME}
+cd "${HOME}" || exit
 wget https://downloads.yugabyte.com/releases/2.15.2.0/yugabyte-2.15.2.0-b87-linux-x86_64.tar.gz
-tar xvfz yugabyte-2.15.2.0-b87-linux-x86_64.tar.gz && cd yugabyte-2.15.2.0/
+tar xvfz yugabyte-2.15.2.0-b87-linux-x86_64.tar.gz && cd yugabyte-2.15.2.0/ || exit
 ./bin/post_install.sh
