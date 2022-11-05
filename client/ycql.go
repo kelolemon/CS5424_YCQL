a4ab59d457d4a6d20daa180b13b26fe416dcc559
kelolemon <cutesinapis@gmail.com>
2022-11-06 01:49:06 +0800

    fix: scipts

--- client/ycql.go
+++ client/ycql.go
@@ -23,9 +23,10 @@ func InitDB() (err error) {
 	dbCluster = gocql.NewCluster(Cluster0IP, Cluster1IP, Cluster2IP, Cluster3IP, Cluster4IP)
 	dbCluster.Keyspace = KeySpace
 	dbCluster.Consistency = gocql.Quorum
-	dbCluster.Timeout = 1000000 * time.Millisecond
+	dbCluster.Timeout = 100000000 * time.Millisecond
+	dbCluster.ConnectTimeout = 100000000 * time.Millisecond
+	dbCluster.MaxWaitSchemaAgreement = 100000000 * time.Millisecond
 	dbCluster.Port = 9142
-
 	Session, err = dbCluster.CreateSession()
 	return err
 }
--- shell/sa.sh
+++ shell/sa.sh
@@ -10,3 +10,17 @@ ssh x3 "./start_master.sh xcnd8.comp.nus.edu.sg" &&
 
 ssh x2 "./start_tserver.sh xcnd7.comp.nus.edu.sg" && \
 ssh x4 "./start_tserver.sh xcnd50.comp.nus.edu.sg"
+
+
+ssh x0 "./t_k.sh"
+ssh x1 "./t_k.sh"
+ssh x2 "./t_k.sh"
+ssh x3 "./t_k.sh"
+ssh x4 "./t_k.sh"
+
+
+ssh x0 "kill -9 $(ps aux | grep "./bin/yb-tserver" | awk '{print $2}')"
+ssh x1 "kill -9 $(ps aux | grep "./bin/yb-tserver" | awk '{print $2}')"
+ssh x2 "kill -9 $(ps aux | grep "./bin/yb-tserver" | awk '{print $2}')"
+ssh x3 "kill -9 $(ps aux | grep "./bin/yb-tserver" | awk '{print $2}')"
+ssh x4 "kill -9 $(ps aux | grep "./bin/yb-tserver" | awk '{print $2}')"
