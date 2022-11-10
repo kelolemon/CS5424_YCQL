25050b250c820a0e616df17960482af6d77898a0
Jiang Luyu <945803847@qq.com>
2022-11-10 21:01:20 +0800

    feat: update new delivery

--- helper/CreateNewDelivery.go
+++ helper/CreateNewDelivery.go
@@ -3,7 +3,6 @@ package helper
 import (
 	"cs5234/common"
 	"cs5234/dao"
-	"fmt"
 	"log"
 	"time"
 )
@@ -14,6 +13,7 @@ func CreateNewDelivery(r common.CreateNewDeliveryReq) (res common.CreateNewDeliv
 		go func(districtID int) {
 			// (a) Let N denote the value of the smallest order number O ID for district (W ID,DISTRICT NO)
 			// with O CARRIER ID = null; i.e.,
+
 			orderRes, err := dao.GetALlOrdersNotDelivery(r.WarehouseID, int32(districtID))
 			if err != nil {
 				log.Printf("[warn] get oldest not delivery error err=%v", err)
@@ -32,7 +32,7 @@ func CreateNewDelivery(r common.CreateNewDeliveryReq) (res common.CreateNewDeliv
 					lastOrderNotDelivery = o
 				}
 			}
-			fmt.Println(lastOrderNotDelivery)
+
 			// (b) Update the order X by setting O CARRIER ID to CARRIER ID
 			err = dao.SetCarrierInfo(r.WarehouseID, int32(districtID), lastOrderNotDelivery, r.CarrierID)
 			if err != nil {
@@ -40,6 +40,7 @@ func CreateNewDelivery(r common.CreateNewDeliveryReq) (res common.CreateNewDeliv
 				errChan <- err
 				return
 			}
+
 			// (c) Update all the order-lines in X by setting OL DELIVERY D to the current date and time
 			orderDeliveryDate := time.Unix(time.Now().Unix(), 0)
 			err = dao.SetOrderLinesDeliveryDate(orderDeliveryDate, r.WarehouseID, int32(districtID), lastOrderNotDelivery.ID)
@@ -48,6 +49,7 @@ func CreateNewDelivery(r common.CreateNewDeliveryReq) (res common.CreateNewDeliv
 				errChan <- err
 				return
 			}
+
 			// Update customer C as follows:
 			//• Increment C BALANCE by B, where B denote the sum of OL AMOUNT for all the items placed in order X
 			//• Increment C DELIVERY CNT by 1
@@ -57,6 +59,7 @@ func CreateNewDelivery(r common.CreateNewDeliveryReq) (res common.CreateNewDeliv
 				errChan <- err
 				return
 			}
+
 			customerRes, err := dao.GetCustomerInfo(lastOrderNotDelivery.CustomerID, r.WarehouseID, int32(districtID))
 
 			if err != nil {
@@ -64,20 +67,23 @@ func CreateNewDelivery(r common.CreateNewDeliveryReq) (res common.CreateNewDeliv
 				errChan <- err
 				return
 			}
+
 			err = dao.SetCustomerBalance(customerRes.ID, r.WarehouseID, int32(districtID), customerRes.Balance+b, customerRes.NumDeliveryMade+1)
 			if err != nil {
 				log.Printf("[warn] update order delivery balance error, err=%v", err)
 				errChan <- err
 				return
 			}
+
 			// update self order by customer table
-			err = dao.SetOrderByCustomerBalanceINfo(customerRes.Balance+b, r.CarrierID, r.WarehouseID, int32(districtID), customerRes.ID, lastOrderNotDelivery.OrderEntryTime)
+			err = dao.SetOrderByCustomerBalanceInfo(customerRes.Balance+b, r.CarrierID, r.WarehouseID, int32(districtID), customerRes.ID, lastOrderNotDelivery.OrderEntryTime)
 			if err != nil {
 				log.Printf("[warn] update order by customer delivery balance error, err=%v", err)
 				errChan <- err
 				return
 			}
 			// update select customer balance table
+
 			customerBalanceRes, err := dao.GetCustomerBalanceInfo(customerRes.ID, r.WarehouseID, int32(districtID))
 			if err != nil {
 				log.Printf("[warn] get customer balance info error, err=%v, costomer=%v", err, customerRes)
@@ -85,12 +91,14 @@ func CreateNewDelivery(r common.CreateNewDeliveryReq) (res common.CreateNewDeliv
 				return
 			}
 			customerBalanceRes.Balance = customerRes.Balance + b
+
 			err = dao.DeleteOrderByCustomerInfo(customerRes.ID, r.WarehouseID, int32(districtID))
 			if err != nil {
 				log.Printf("[warn] delete customer balance info error, err=%v", err)
 				errChan <- err
 				return
 			}
+
 			err = dao.InsertCustomerBalanceInfo(&customerBalanceRes)
 			if err != nil {
 				log.Printf("[warn] create customer balance info error, err=%v", err)
@@ -100,11 +108,13 @@ func CreateNewDelivery(r common.CreateNewDeliveryReq) (res common.CreateNewDeliv
 			errChan <- nil
 		}(districtID)
 	}
+
 	for i := 1; i <= 10; i++ {
 		if err := <-errChan; err != nil {
 			log.Printf("[error] create new delivery error err=%v", err)
 			return common.CreateNewDeliveryResp{}, err
 		}
 	}
+
 	return common.CreateNewDeliveryResp{}, nil
 }
